b39e095c603f9316b160ecf2cadb0338949710d3
ACER <ACER@192.168.2.4>
2018-07-22 16:12:21 +0530

    First push

---
+++ SampleProject/src/TestPackage/SampleProgramme.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class SampleProgramme 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("The script runs successfully");
+	}
+
+}
